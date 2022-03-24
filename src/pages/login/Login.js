edf678f20a8c8e49af240fbe6a05cc72f13a3427
waleed-sabir <waleed_sabir@yahoo.com>
2022-03-24 13:15:33 +0500

    updated some css styles

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -15,33 +15,41 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="auth-form">
-      <h2>login</h2>
-      <label>
-        <span>email:</span>
-        <input
-          required
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input
-          required
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      {!isPending && <button className="btn">Log in</button>}
-      {isPending && (
-        <button className="btn" disabled>
-          loading
-        </button>
-      )}
-      {error && <div className="error">{error}</div>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="auth-form">
+        <h2>login</h2>
+        <label>
+          <span>email:</span>
+          <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input
+            required
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        {!isPending && <button className="btn">Log in</button>}
+        {isPending && (
+          <button className="btn" disabled>
+            loading
+          </button>
+        )}
+        {error && <div className="error">{error}</div>}
+      </form>
+      <p className="login-note">
+        <strong>NOTE:</strong> For testing purpose, one can{" "}
+        <strong>LOGIN</strong> with the given credentials i.e. Email{" "}
+        <strong>'mario@netninja.dev'</strong> and password{" "}
+        <strong>'mario123'</strong>.
+      </p>
+    </>
   );
 }
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -41,47 +41,55 @@ export default function Signup() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="auth-form">
-      <h2>sign up</h2>
-      <label>
-        <span>email:</span>
-        <input
-          required
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input
-          required
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <label>
-        <span>display name:</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setDisplayName(e.target.value)}
-          value={displayName}
-        />
-      </label>
-      <label>
-        <span>Profile thumbnail:</span>
-        <input required type="file" onChange={handleFileChange} />
-        {thumbnailError && <div className="error">{thumbnailError}</div>}
-      </label>
-      {!isPending && <button className="btn">Sign up</button>}
-      {isPending && (
-        <button className="btn" disabled>
-          loading
-        </button>
-      )}
-      {error && <div className="error">{error}</div>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="auth-form">
+        <h2>sign up</h2>
+        <label>
+          <span>email:</span>
+          <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input
+            required
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label>
+          <span>display name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+        <label>
+          <span>Profile thumbnail:</span>
+          <input required type="file" onChange={handleFileChange} />
+          {thumbnailError && <div className="error">{thumbnailError}</div>}
+        </label>
+        {!isPending && <button className="btn">Sign up</button>}
+        {isPending && (
+          <button className="btn" disabled>
+            loading
+          </button>
+        )}
+        {error && <div className="error">{error}</div>}
+      </form>
+      <p className="login-note">
+        <strong>NOTE:</strong> For testing purpose, one can{" "}
+        <strong>LOGIN</strong> with the given credentials i.e. Email{" "}
+        <strong>'mario@netninja.dev'</strong> and password{" "}
+        <strong>'mario123'</strong>.
+      </p>
+    </>
   );
 }
